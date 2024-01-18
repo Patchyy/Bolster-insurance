a9fa8a7358244949a904c84d3a43a691dda8b627
Wesley van Gogh <wesley@gethooked.nl>
2024-01-18 16:27:50 +0100

    stepper names

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,45 +1,39 @@
 const Form = () => {
-  return (
-    <div>
-      <div id="formulier" className="bg-white-off h-screen">
-     
-      <div className="header bg-blue-dark w-full h-56 z-0 flex flex-row items-center justify-center">
-      <ol className="flex items-center w-1/2 relative">
-     
-    <li className="flex w-full items-center  justify-center  relative after:content-[''] after:w-full after:h-1 after:border-b after:border-pink after:border-4 after:inline-block">
-        <span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold">
-            1
-        </span>
-        <span className="text-white absolute -top-1/2  text-center">Value estimate</span>
-    </li>
-   
-    <li className="flex w-full items-center relative after:content-[''] after:w-full after:h-1 after:border-b after:border-pink after:border-4 after:inline-block ">
-        <span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-2xl font-bold">
-            2
-        </span>
-        <span className="text-white absolute -top-1/2 -translate-x-1/2 text-center">Additonal info</span>
-    </li>
-    <li className="flex w-full items-center relative after:content-[''] after:w-full after:h-1 after:border-b after:border-pink after:border-4 after:inline-block ">
-        <span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-2xl font-bold">
-            3
-        </span>
-        <span className="text-white absolute -top-1/2 -translate-x-1/2 text-center">special objects</span>
-    </li>
-    <li className="flex items-center justify-center">
-        <span className="flex items-center text-2xl font-bold justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0">
-            4
-        </span>
-        <span className="text-white absolute -top-1/2  text-center">Insurance</span>
-    </li>
-</ol>
-      </div>
-      <div className="xl:container md:mx-auto bg-white h-screen">
-   
-        
-      </div>
-    </div>
-    </div>
-  );
+	return (
+		<div>
+			<div id="formulier" className=" h-screen">
+				<div className="header py-12 relative bg-blue-dark w-full  -z-[2] flex flex-row items-center justify-center ">
+					<ol className="flex  items-center w-1/2 relative gap-8 ">
+						<li className="flex w-full items-center  flex-col  relative after:content-[''] after:w-[120%] after:h-1 after:border-b after:border-pink after:border-4 after:inline-block after:absolute after:left-1/2 after:top-1/2  after:-z-[1] ">
+							<span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								1
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+								Value estimate
+							</span>
+						</li>
+						<li className="flex w-full items-center  flex-col  relative after:content-[''] after:w-[120%] after:h-1 after:border-b after:border-pink after:border-4 after:inline-block after:absolute after:left-1/2 after:top-1/2  after:-z-[1] ">
+							<span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								2
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+								Value estimate
+							</span>
+						</li>
+						<li className="flex w-full items-center  flex-col  relative ">
+							<span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								3
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+								Value estimate
+							</span>
+						</li>
+					</ol>
+				</div>
+				<div className="xl:container mx-auto bg-white h-screen"></div>
+			</div>
+		</div>
+	);
 };
 
 export default Form;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    colors: {
-      'blue-dark': '#02004E',
-      blue: '#486AE9',
-      pink: '#FF6587',
-      green: '#00C88B',
-      white: '#FFFFFF',
-      black: '#000000',
-      'white-off': '#F9F7F3',
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
+	theme: {
+		screens: {
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
+
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
+		colors: {
+			'blue-dark': '#02004E',
+			blue: '#486AE9',
+			pink: '#FF6587',
+			green: '#00C88B',
+			white: '#FFFFFF',
+			black: '#000000',
+			'white-off': '#F9F7F3',
+		},
+		extend: {},
+	},
+	plugins: [],
 };
