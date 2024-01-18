8c98c63a2a0adffb6e32ad298d259ba73c3b9199
Stan <45259674+Patchyy@users.noreply.github.com>
2024-01-18 17:01:56 +0100

    FIX: fixed header on form page

--- src/pages/Dashboard/DashboardDetail.view.tsx
+++ src/pages/Dashboard/DashboardDetail.view.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { IShipment } from "../../types/all";
+
 const DashboardDetailPage = () => {
+  const [shipments, setShipments] = useState<IShipment[]>([]);
+
+  useEffect(() => {
+    const getShipments = async () => {
+      const res = await fetch('http://localhost:5000/shipments');
+      const data = await res.json();
+
+      setShipments(data);
+    };
+
+    getShipments();
+  }, []);
+
+  useEffect(() => {
+    console.log('Shipments:', shipments);
+  }, [shipments]);
   return (
     <div id="shipments" className="bg-white-off h-[400px]">
       <h1>Details</h1>
+        
     </div>
   );
 };
--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,44 +1,46 @@
 const Form = () => {
   return (
     <div>
-      <div id="formulier" className="bg-white-off h-screen">
-     
-      <div className="header bg-blue-dark w-full h-56 z-0 flex flex-row items-center justify-center">
-      <ol className="flex items-center w-1/2 relative">
-     
-    <li className="flex w-full items-center  justify-center  relative after:content-[''] after:w-full after:h-1 after:border-b after:border-pink after:border-4 after:inline-block">
-        <span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold">
-            1
-        </span>
-        <span className="text-white absolute -top-1/2  text-center">Value estimate</span>
-    </li>
-   
-    <li className="flex w-full items-center relative after:content-[''] after:w-full after:h-1 after:border-b after:border-pink after:border-4 after:inline-block ">
-        <span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-2xl font-bold">
-            2
-        </span>
-        <span className="text-white absolute -top-1/2 -translate-x-1/2 text-center">Additonal info</span>
-    </li>
-    <li className="flex w-full items-center relative after:content-[''] after:w-full after:h-1 after:border-b after:border-pink after:border-4 after:inline-block ">
-        <span className="flex items-center justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-2xl font-bold">
-            3
-        </span>
-        <span className="text-white absolute -top-1/2 -translate-x-1/2 text-center">special objects</span>
-    </li>
-    <li className="flex items-center justify-center">
-        <span className="flex items-center text-2xl font-bold justify-center w-10 h-10 bg-white rounded-full lg:h-16 lg:w-16  shrink-0">
-            4
-        </span>
-        <span className="text-white absolute -top-1/2  text-center">Insurance</span>
-    </li>
-</ol>
-      </div>
-      <div className="xl:container md:mx-auto bg-white h-screen">
-   
-        
-      </div>
-    </div>
-    </div>
+			<div id="formulier" className=" h-screen bg-white-off">
+				<div className="header py-12 relative bg-blue-dark w-full  z-10 flex flex-row items-center justify-center ">
+					<ol className="flex  items-center w-1/2 relative gap-8 z-10">
+						<li className="flex w-full items-center  flex-col relative after:content-[''] after:w-[120%] after:h-1 after:border-b after:border-pink after:border-4 after:inline-block after:absolute after:left-1/2 after:top-1/2  after:-z-[1] ">
+							<span className="flex items-center justify-center w-16 h-16 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								1
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+								Value estimate
+							</span>
+						</li>
+						<li className="flex w-full items-center  flex-col  relative after:content-[''] after:w-[120%] after:h-1 after:border-b after:border-pink after:border-4 after:inline-block after:absolute after:left-1/2 after:top-1/2  after:-z-[1] ">
+							<span className="flex items-center justify-center w-16 h-16 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								2
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+								Additonal information
+							</span>
+						</li>	
+            <li className="flex w-full items-center  flex-col  relative after:content-[''] after:w-[120%] after:h-1 after:border-b after:border-pink after:border-4 after:inline-block after:absolute after:left-1/2 after:top-1/2  after:-z-[1] ">
+							<span className="flex items-center justify-center w-16 h-16 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								3
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+                  Special objects
+							</span>
+						</li>
+						<li className="flex w-full items-center  flex-col  relative ">
+							<span className="flex items-center justify-center w-16 h-16 bg-white rounded-full lg:h-16 lg:w-16  shrink-0 text-black text-2xl font-bold ">
+								4
+							</span>
+							<span className="text-white absolute -top-1/2  ">
+								Insurances
+							</span>
+						</li>
+					</ol>
+				</div>
+				<div className="xl:container mx-auto bg-white h-screen"></div>
+			</div>
+		</div>
   );
 };
 
