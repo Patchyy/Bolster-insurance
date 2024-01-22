fc2f756dcd34306e7c3b121bd0c9942ea2263726
StanSteenis <stansteenis@happyhorizon.com>
2024-01-22 16:50:23 +0100

    FEAT: Started with value slider

--- src/components/form/Formsteps/ValueEstimate.tsx
+++ src/components/form/Formsteps/ValueEstimate.tsx
@@ -64,7 +64,39 @@ const ValueEstimate = () => {
           );
         })}
       </div>
-      {}
+      <h1 className="text-3xl text-center mx-auto mt-[48px] mb-[48px] font-spoof w-2/3">
+        Based on your given information we estimate your house hold effects are
+        worth
+      </h1>
+
+      <div className="relative mb-6">
+        <label
+          htmlFor="large-range"
+          className="block mb-2 text-xl font-medium text-blue text-center"
+        >
+          € 15000,-
+        </label>
+        <input
+          id="large-range"
+          type="range"
+          value="1000"
+          min="100"
+          max="1500"
+          className="w-full h-4 bg-white-off rounded-lg appearance-none cursor-pointer range-lg "
+        />
+        <span className="text-sm text-gray-500 absolute start-0 -bottom-6">
+          Min $100
+        </span>
+        <span className="text-sm text-gray-black  absolute start-1/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">
+          $500
+        </span>
+        <span className="text-sm text-black absolute start-2/3 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">
+          $1000
+        </span>
+        <span className="text-sm text-black  absolute end-0 -bottom-6">
+          Max $1500
+        </span>
+      </div>
     </div>
   );
 };
